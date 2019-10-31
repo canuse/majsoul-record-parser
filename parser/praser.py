28905db51ba703b3b2ed5208090e517a3a934cbb
canuse <44812363+canuse@users.noreply.github.com>
2019-10-31 23:36:41 +0800

    parse new round

--- parser/praser.py
+++ parser/praser.py
@@ -1,4 +1,5 @@
-from .record import *
+from record import *
+
 
 class scanner:
     def __init__(self, data):
@@ -19,10 +20,10 @@ class parser:
 
     def getVariant(self):
         variant = []
-        tmp = self.scanner.fetch()
+        tmp = self.scanner.fetch()[0]
         variant.append(tmp)
-        while tmp[0] >= 128:
-            tmp = self.scanner.fetch()
+        while tmp >= 128:
+            tmp = self.scanner.fetch()[0]
             variant.append(tmp)
         return self.variantToInt(variant)
 
@@ -35,6 +36,12 @@ class parser:
                 value += variant[i] * (128 ** i)
         return value
 
+    def getType(self):
+        val = self.scanner.fetch()[0]
+        field = val >> 3
+        type = val - (field << 3)
+        return field, type
+
 
 class parseGame(parser):
     def __init__(self, filename):
@@ -42,18 +49,92 @@ class parseGame(parser):
             self.protoData = file.read()
         self.scanner = scanner(self.protoData)
         super().__init__(self.scanner)
-        self.gameRecord=Game()
+        self.gameRecord = Game()
+
+    def parse(self):
+        field, type = self.getType()
+        if field!=1 or type!=2:
+            raise RuntimeError()
+        length = self.getVariant()
+        self.scanner.fetch(length)
+        field, type = self.getType()
+        if field!=2 or type!=2:
+            raise RuntimeError()
+        length = self.getVariant()
+        data = self.scanner.fetch(length)
+        newRound = parseRound(data)
+        newRound.parse()
 
 
-class praseRound(parser):
+class parseRound(parser):
     def __init__(self, data):
         self.scanner = scanner(data)
         super().__init__(self.scanner)
-        self.roundRecord=None
+        self.roundRecord = None
+
+    def parse(self):
+        field, type = self.getType()
+        if field != 1 or type != 2:
+            raise RuntimeError()
+        length = self.getVariant()
+        # start game
+        tsc=parser(scanner(self.scanner.fetch(length)))
+        field, type = tsc.getType()
+        if field != 1 or type != 2:
+            raise RuntimeError()
+        tsc.scanner.fetch(tsc.getVariant())
+        field, type = tsc.getType()
+        if field != 2 or type != 2:
+            raise RuntimeError()
+        length = tsc.getVariant()
+        tsc = parser(scanner(tsc.scanner.fetch(length)))
+        field, type = tsc.getType()
+        chang = tsc.getVariant()
+        field, type = tsc.getType()
+        ju = tsc.getVariant()
+        field, type = tsc.getType()
+        ben = tsc.getVariant()
+        field, type = tsc.getType()
+        while field!=7:
+            length = tsc.getVariant()
+            tsc.scanner.fetch(length)
+            field, type = tsc.getType()
+        ttile=[]
+        handtile=[]
+        while field==7:
+            length = tsc.getVariant()
+            ttile.append(tsc.scanner.fetch(length).decode())
+            field, type = tsc.getType()
+        handtile.append(ttile)
+        ttile = []
+        while field==8:
+            length = tsc.getVariant()
+            ttile.append(tsc.scanner.fetch(length).decode())
+            field, type = tsc.getType()
+        handtile.append(ttile)
+        ttile = []
+        while field == 9:
+            length = tsc.getVariant()
+            ttile.append(tsc.scanner.fetch(length).decode())
+            field, type = tsc.getType()
+        handtile.append(ttile)
+        if field==10:
+            ttile = []
+            while field == 10:
+                length = tsc.getVariant()
+                ttile.append(tsc.scanner.fetch(length).decode())
+                field, type = tsc.getType()
+            handtile.append(ttile)
+        thisround=Round(chang,ju,ben,handtile)
+        print(handtile)
 
 
-class praseitem(parser):
+class parseitem(parser):
     def __init__(self, data):
         self.scanner = scanner(data)
         super().__init__(self.scanner)
         self.itemRecord = None
+
+if __name__=="__main__":
+    a=parseGame('../test/1')
+    a.parse()
