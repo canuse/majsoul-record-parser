eb6967ed948160f73bdd8c700282a0a5c52e0b95
canuse <44812363+canuse@users.noreply.github.com>
2019-11-09 17:19:05 +0800

    change tile order

--- majsoul/tile.py
+++ majsoul/tile.py
@@ -6,15 +6,46 @@ class Tile:
                 self.value = 5
             else:
                 self.value = int(tile[0])
-        elif 's' in tile:
+        elif 'p' in tile:
             if tile[0] == '0':
                 self.value = 15
             else:
                 self.value = int(tile[0]) + 10
-        elif 'p' in tile:
+        elif 's' in tile:
             if tile[0] == '0':
                 self.value = 25
             else:
                 self.value = int(tile[0]) + 20
         else:
             self.value = int(tile[0]) + 30
+
+    @staticmethod
+    def tileToValue(tile):
+        if 'm' in tile:
+            if tile[0] == '0':
+                return 5
+            else:
+                return int(tile[0])
+        elif 'p' in tile:
+            if tile[0] == '0':
+                return 15
+            else:
+                return int(tile[0]) + 10
+        elif 's' in tile:
+            if tile[0] == '0':
+                return 25
+            else:
+                return int(tile[0]) + 20
+        else:
+            return int(tile[0]) + 30
+
+    @staticmethod
+    def valueToTile(value):
+        if value < 10:
+            return str(value) + 'm'
+        elif value < 20:
+            return str(value - 10) + 'p'
+        elif value < 30:
+            return str(value - 20) + 's'
+        else:
+            return str(value - 30) + 'z'
